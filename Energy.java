4a28f54a09ee48d334d391be4d76f96559a78fed
Ubuntu <ubuntu@ip-172-31-82-219.ec2.internal>
2021-11-23 03:49:45 +0000

    try

---
+++ Energy.java
@@ -0,0 +1,69 @@
+/*
+* This program gets mass of object from the user,
+* calculates and show how much energy could be released from that object.
+*
+* @author  Donggeun Lim
+* @version 1.0
+* @since   2021-11-20
+*/
+
+import java.util.Scanner;
+
+/**
+* This is the standard calculated energy program.
+*/
+public final class Energy {
+    // Constant
+    /**
+    * Speed of light: 2.998*10^8 m/s.
+    */
+    static final double SPEED_OF_LIGHT = 2.998e8;
+    /**
+    * A thousand tons of TNT would release about 4 TJ of energy (TJ: 10^12J).
+    */
+    static final double POWER_OF_BOMB = 4.0e12;
+
+    /**
+    * Prevent instantiation.
+    * Throw an exception IllegalStateException.
+    * if this ever is called
+    *
+    * @throws IllegalStateException
+    *
+    */
+    private Energy() {
+        // Prevent instantiation
+        // Optional: throw an exception e.g. AssertionError
+        // if this ever *is* called
+        throw new IllegalStateException("Cannot be instantiated");
+    }
+
+    /**
+    * The starting main() function.
+    *
+    * @param args No args will be used
+    */
+    public static void main(final String[] args) {
+        final Scanner myObj = new Scanner(System.in);
+
+        System.out.print("Enter the mass of an object in kilograms: ");
+        try {
+            final float massOfObject = myObj.nextFloat();
+
+            // Process
+            final float amountOfEnergy = massOfObject
+                           * (float) Math.pow(SPEED_OF_LIGHT, 2);
+            final float equivalentBomb = amountOfEnergy / (float) POWER_OF_BOMB;
+
+            // Output
+            System.out.println("If " + massOfObject
+                + " kg of mass was converted to energy, it would produce "
+                + amountOfEnergy + "J of energy.\nOr this would be "
+                + "equivalent to a " + equivalentBomb + " kiloton bomb.");
+        }
+        catch (java.util.InputMismatchException errorCode) {
+            System.err.print("Invalid input");
+        }
+    }
+}
+
---
+++ package-info.java
@@ -0,0 +1,9 @@
+/*
+This program gets mass of object from the user's input,
+calculates and display energy of object in Joules and
+tells input is not a number, if inout is not a number
+author  Donggeun Lim
+version 1.0
+since   2021-11-21
+*/
+package ca.mths.unit1.unit02.java.main;
